c1080d83bbf330d5c7abf45d56c1be2fd6566941
viboof <make@boof.work>
2025-03-11 17:30:49 -0700

    this fucking sucks so bad but it works

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@
  */
 
 const PDX_ROLE = "<@&475812819191726111>";
+const BOOF = "<@848762953184444467>";
 
-const DATA_HEADER = "<script type=application/ld+json>";
+const DATA_HEADER = '<script type=application/ld+json>{"@context":"http://schema.org","@type":"Event",';
 const DATA_FOOTER = "</script>";
 const MODA = "https://www.modaportland.com/events/";
 
@@ -33,19 +34,24 @@ export default {
 			return new Response("no");
 		};
 
+		if (query.has("ui")) {
+			const url = query.get("url");
+			return new Response(`<a href="${url}">try again</a><br><br><a href="${url}&approved=1">approve</a>`, { headers: { "Content-Type": "text/html" } });
+		}
+
 		const date = query.has("date") 
 			? query.get("date") 
 			: getCurrentDateString();
 
 		const moda = query.has("moda") ? query.get("moda") : MODA;
 
-		await this.run(date, moda, env);
+		await this.run(date, moda, env, query.has("approved"));
 
 		return new Response("ok");
 	},
 
 	async scheduled(_, env, __) {
-		await this.run(getCurrentDateString(), MODA, env);
+		await this.run(getCurrentDateString(), MODA, env, false);
 	},
 
 	async discord(url, message, image = "") {
@@ -66,25 +72,35 @@ export default {
 		})
 	},
 
-	async run(date, moda, env) {
-		const res = await fetch(moda);
-		const text = await res.text();
+	async run(date, moda, env, approved = false) {
+		try {
+			const res = await fetch(moda);
+			const text = await res.text();
 
-		console.log("text:", text);
+			console.log("text:", text);
 
-		const parts = text.split(DATA_HEADER);
+			const parts = text.split(DATA_HEADER);
 
-		// part 0 will be the web page before the first script,
-		// part 1 will be the nearest upcoming event (or today),
-		// part 2... will be further events we don't care about
-		const event = JSON.parse(parts[1].split(DATA_FOOTER)[0]);
+			// part 0 will be the web page before the first script,
+			// part 1 will be the nearest upcoming event (or today),
+			// part 2... will be further events we don't care about
+			const event = JSON.parse("{" + parts[1].split(DATA_FOOTER)[0]);
 
-		const discordUrl = env.DISCORD_WEBHOOK_URL;
+			console.log("event:", event);
 
-		if (event.startDate.startsWith(date)) {
-			await this.discord(discordUrl, `# ${PDX_ROLE} **${event.name}** at the Moda Center today <:ASSGUN:1004546251850788884>\n(${date})`, "dontpark.png");
-		} else {
-			await this.discord(discordUrl, `nothing at the Moda Center today (${date}) <:ASSJKWON:1012402700438208652>`);
+			const discordUrl = approved ? env.DISCORD_WEBHOOK_URL : env.APPROVAL_DISCORD_WEBHOOK_URL;
+			const url = env.URL + "?key=" + env.SECRET_KEY + "&date=" + date + "&moda=" + encodeURIComponent(moda);
+			const approvalText = `\n\ndecide: ${url}&ui=1&url=${encodeURIComponent(url)}${BOOF}`;
+			const suffix = approved ? "" : approvalText;
+
+			if (event.startDate.startsWith(date)) {
+				await this.discord(discordUrl, `# ${PDX_ROLE} **${event.name}** at the Moda Center today <:ASSGUN:1004546251850788884>\n(${date})${suffix}`, "dontpark.png");
+			} else {
+				await this.discord(discordUrl, `nothing at the Moda Center today (${date}) <:ASSJKWON:1012402700438208652>${suffix}`);
+			}
+		} catch (e) {
+			await this.discord(env.APPROVAL_DISCORD_WEBHOOK_URL, BOOF + " " + e + "\n" + e.stack);
+			throw e;
 		}
 	},
 };
